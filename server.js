84b23729ba4516c2a00188a6fcd889c13e44ff54
Duc Anh <ducanh.le1908@gmail.com>
2022-07-21 22:30:05 +0700

    update

---
+++ server.js
@@ -0,0 +1,85 @@
+const mysql = require('mysql');
+const http = require('http');
+const fs = require('fs');
+const qs = require('qs');
+const url = require('url');
+
+
+const configSQL = {
+    host : 'localhost',
+    user : 'root',
+    password : '123456',
+    database : 'product_manager',
+    charset : 'utf8_general_ci'
+}
+
+const connection = mysql.createConnection(configSQL);
+
+function getProduct() {
+    let sql = "SELECT * FROM products"
+}
+
+connection.connect((err)=>{
+    if(err) {
+        console.log(err);
+    }
+    else
+    {
+        console.log('Connect Success!!');
+    }
+})
+
+const server = http.createServer((req,res)=>{
+    let urlParse = url.parse(req.url);
+    let path = urlParse.pathname;
+    
+
+    switch(path) {
+        case '/' : {
+            fs.readFile('views/index.html','utf-8',(err,data)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.writeHead(200, {'Content-type':'text/html'});
+                    res.write(data);
+                    res.end();
+                }
+            });
+            break;
+        }
+        case '/product':{
+            fs.readFile('views/products/list.html','utf-8',(err,data)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.writeHead(200, {'Content-type':'text/html'});
+                    res.write(data);
+                    res.end();
+                }
+            });
+            break;
+        }
+
+        default :{
+            fs.readFile('views/error-404.html','utf-8',(err,data)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.writeHead(200, {'Content-type':'text/html'});
+                    res.write(data);
+                    res.end();
+                }
+            })
+            break;
+        }
+    }
+
+    
+
+})
+server.listen(8080, ()=>{
+    console.log('Server is running http://localhost:3000');
+})
