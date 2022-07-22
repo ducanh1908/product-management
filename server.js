a838ed25c47bf0a265e584ab3940af3166764a32
Duc Anh <ducanh.le1908@gmail.com>
2022-07-22 20:46:12 +0700

    update product case

--- server.js
+++ server.js
@@ -84,6 +84,14 @@ const server = http.createServer((req,res)=>{
             });
             break;
         }
+        case '/product/create': {
+
+            break;
+        }
+        case '/product/edit': {
+
+            break;
+        }
 
         default :{
             fs.readFile('views/error-404.html','utf-8',(err,data)=>{
@@ -105,4 +113,4 @@ const server = http.createServer((req,res)=>{
 })
 server.listen(8080, ()=>{
     console.log('Server is running http://localhost:3000');
-})
+});
